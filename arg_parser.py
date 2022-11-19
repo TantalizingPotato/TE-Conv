458051b49d9edd7e4353637e37ea1f88eb67635c
TantalizingPotato <zhglgh@163.com>
2022-11-20 01:50:15 +0800

    add new args

--- arg_parser.py
+++ arg_parser.py
@@ -1,6 +1,16 @@
 import argparse
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    if v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 def get_args():
     parser = argparse.ArgumentParser('Interface for TE-Conv framework')
 
@@ -9,9 +19,9 @@ def get_args():
     parser.add_argument("--mode", type=str, default="tgn",
                         choices=["tgn", "dyrep", "tgat", "jodie", "distance_encoding"],
                         help="the model name, choices=[\"tgn\", \"dyrep\", \"tgat\", \"jodie\", \"distance_encoding\"]")
-    parser.add_argument("--use_embedding", type=bool, default=True,
+    parser.add_argument("--use_embedding", type=str2bool, default=True,
                         help="for tgn only, using gnn embedding or not")
-    parser.add_argument("--use_memory_embedding", type=bool, default=False,
+    parser.add_argument("--use_memory_embedding", type=str2bool, default=False,
                         help="for distance_encoding only, concatenating memory embedding to "
                              "initial node embedding or not")
     parser.add_argument("--name", type=str, default="wikipedia",
@@ -37,17 +47,17 @@ def get_args():
                         help="the dimension number of time encoding")
     parser.add_argument("--embedding_dim", type=int, default=100,
                         help="the dimension number of finally obtained node embedding")
-    parser.add_argument("--time_encoding", type=bool, default=True,
+    parser.add_argument("--time_encoding", type=str2bool, default=True,
                         help="whether to use time encoding")
-    parser.add_argument("--distance_encoding", type=bool, default=False,
+    parser.add_argument("--distance_encoding", type=str2bool, default=False,
                         help="")
-    parser.add_argument("--de_shortest_path", type=bool, default=True,
+    parser.add_argument("--de_shortest_path", type=str2bool, default=True,
                         help="for distance_encoding only, whether to use shortest_path")
-    parser.add_argument("--de_random_walk", type=bool, default=True,
+    parser.add_argument("--de_random_walk", type=str2bool, default=True,
                         help="for distance encoding only, whether to use random_walk")
-    parser.add_argument("--edge_attribute", type=bool, default=False,
+    parser.add_argument("--edge_attribute", type=str2bool, default=False,
                         help="whether to use edge attribute when generating node embedding")
-    parser.add_argument("--use_msg", type=bool, default=False,
+    parser.add_argument("--use_msg", type=str2bool, default=False,
                         help="whether to load message (edge attribute) from file")
     parser.add_argument("--n_layer", type=int, default=1,
                         help="number of layers in the GNN model")
@@ -56,10 +66,16 @@ def get_args():
                         help="number of negative samples (that correspond to one positive sample)")
     parser.add_argument("--n_coefficient", type=int, default=2,
                         help="loss weight for negative sample loss")
+    parser.add_argument("--mse_loss_coefficient", type=int, default=2,
+                        help="loss weight for the mse loss used in time prediction")
 
-    parser.add_argument("--dyrep_loss", type=bool, default=True,
+
+    parser.add_argument("--dyrep_loss", type=str2bool, default=False,
                         help="whether to adopt dyrep-style loss function")
-    parser.add_argument("--use_dyrepabstractsum", type=bool, default=True,
+    # parser.add_argument("--other_loss", default="time_prediction", choices=["link_prediction", "time_prediction"],
+    #                     help="the adopted loss function (if --dyrep_loss is False),"
+    #                          "choices=[\"link_prediction\", \"time_prediction\"].")
+    parser.add_argument("--use_dyrepabstractsum", type=str2bool, default=True,
                         help="whether to adopt dyrep-style time predictor")
 
     parser.add_argument("--num_basis", type=int, default=64,
@@ -68,29 +84,31 @@ def get_args():
                         help="monte carlo sample number for survival term in time predictor")
     parser.add_argument("--mc_num_time", type=int, default=100,
                         help="monte carlo sample number for computing some integral expression in time predictor")
+    parser.add_argument("--basis", default="powerlaw", choices=["exp", "sigmoid", "cos", "relu", "powerlaw", "mixed"],
+                        help="the type of basis function adopted in time predictor")
 
-    parser.add_argument("--use_neighbor_finder", type=bool, default=True,
+    parser.add_argument("--use_neighbor_finder", type=str2bool, default=True,
                         help="whether to use neighbor_finder (if set to True) or neighbor_loader (if set to False)")
 
-    parser.add_argument("--use_neg", type=bool, default=False,
+    parser.add_argument("--use_neg", type=str2bool, default=False,
                         help="whether to insert negative samples into batches")
-    parser.add_argument("--sample_neg_dst_strictly", type=bool, default=True,
+    parser.add_argument("--sample_neg_dst_strictly", type=str2bool, default=True,
                         help="whether to sample negative dst nodes strictly (avoiding coincident positive samples)")
-    parser.add_argument("--can_see_neighbors_in_same_batch", type=bool, default=True,
+    parser.add_argument("--can_see_neighbors_in_same_batch", type=str2bool, default=True,
                         help="whether the model can see a node's temporal neighbors that is in the same batch with it")
-    parser.add_argument("--only_predict_last_events_time", type=bool, default=True,
+    parser.add_argument("--only_predict_last_events_time", type=str2bool, default=True,
                         help="whether only to predict the last time of the same events in a batch")
 
-    parser.add_argument("--use_memory", type=bool, default=True,
+    parser.add_argument("--use_memory", type=str2bool, default=True,
                         help="whether to use memory vectors")
 
     parser.add_argument("--neighbor_size", type=int, default=10,
                         help="the number of a node's neighbors returned by neighbor_finder or neighbor_loader")
 
-    parser.add_argument("--dataset_size", type=int, default=2000,
+    parser.add_argument("--dataset_size", type=int, default=200000,
                         help="the number of samples to draw from the dataset")
 
-    parser.add_argument("--cpu_only", type=bool, default=True,
+    parser.add_argument("--cpu_only", type=str2bool, default=True,
                         help="whether to run the model only on CPU (to run without GPU)")
 
     parser.add_argument("--candidates_from", type=str, default="all_dst", choices=["all_dst", "all_nodes", "neighbors"],
